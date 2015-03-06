a1a816a92b3248bb71906e71d16916b41d34ae1c
Rusty Wagner <dontpanic@binaryninja.com>
2015-03-06 03:44:09 -0500

    Implement the other syscalls

--- decree-user/syscall.c
+++ decree-user/syscall.c
@@ -19,6 +19,7 @@
 #define _ATFILE_SOURCE
 #include "qemu-common.h"
 #include "qemu.h"
+#include <sys/mman.h>
 
 
 #define ERRNO_TABLE_SIZE 1200
@@ -100,6 +101,137 @@ void syscall_init(void)
     }
 }
 
+static inline abi_long copy_from_user_fdset(fd_set *fds,
+                                            abi_ulong target_fds_addr,
+                                            int n)
+{
+    int i, nw, j, k;
+    abi_ulong b, *target_fds;
+
+    nw = (n + TARGET_ABI_BITS - 1) / TARGET_ABI_BITS;
+    if (!(target_fds = lock_user(VERIFY_READ,
+                                 target_fds_addr,
+                                 sizeof(abi_ulong) * nw,
+                                 1)))
+        return -TARGET_EFAULT;
+
+    FD_ZERO(fds);
+    k = 0;
+    for (i = 0; i < nw; i++) {
+        /* grab the abi_ulong */
+        __get_user(b, &target_fds[i]);
+        for (j = 0; j < TARGET_ABI_BITS; j++) {
+            /* check the bit inside the abi_ulong */
+            if ((b >> j) & 1)
+                FD_SET(k, fds);
+            k++;
+        }
+    }
+
+    unlock_user(target_fds, target_fds_addr, 0);
+
+    return 0;
+}
+
+static inline abi_ulong copy_from_user_fdset_ptr(fd_set *fds, fd_set **fds_ptr,
+                                                 abi_ulong target_fds_addr,
+                                                 int n)
+{
+    if (target_fds_addr) {
+        if (copy_from_user_fdset(fds, target_fds_addr, n))
+            return -TARGET_EFAULT;
+        *fds_ptr = fds;
+    } else {
+        *fds_ptr = NULL;
+    }
+    return 0;
+}
+
+static inline abi_long copy_to_user_fdset(abi_ulong target_fds_addr,
+                                          const fd_set *fds,
+                                          int n)
+{
+    int i, nw, j, k;
+    abi_long v;
+    abi_ulong *target_fds;
+
+    nw = (n + TARGET_ABI_BITS - 1) / TARGET_ABI_BITS;
+    if (!(target_fds = lock_user(VERIFY_WRITE,
+                                 target_fds_addr,
+                                 sizeof(abi_ulong) * nw,
+                                 0)))
+        return -TARGET_EFAULT;
+
+    k = 0;
+    for (i = 0; i < nw; i++) {
+        v = 0;
+        for (j = 0; j < TARGET_ABI_BITS; j++) {
+            v |= ((abi_ulong)(FD_ISSET(k, fds) != 0) << j);
+            k++;
+        }
+        __put_user(v, &target_fds[i]);
+    }
+
+    unlock_user(target_fds, target_fds_addr, sizeof(abi_ulong) * nw);
+
+    return 0;
+}
+
+static inline abi_long copy_from_user_timeval(struct timeval *tv,
+                                              abi_ulong target_tv_addr)
+{
+    struct target_timeval *target_tv;
+
+    if (!lock_user_struct(VERIFY_READ, target_tv, target_tv_addr, 1))
+        return -TARGET_EFAULT;
+
+    __get_user(tv->tv_sec, &target_tv->tv_sec);
+    __get_user(tv->tv_usec, &target_tv->tv_usec);
+
+    unlock_user_struct(target_tv, target_tv_addr, 0);
+
+    return 0;
+}
+
+/* do_select() must return target values and target errnos. */
+static abi_long do_select(int n,
+                          abi_ulong rfd_addr, abi_ulong wfd_addr,
+                          abi_ulong target_tv_addr)
+{
+    fd_set rfds, wfds;
+    fd_set *rfds_ptr, *wfds_ptr;
+    struct timeval tv, *tv_ptr;
+    abi_long ret;
+
+    ret = copy_from_user_fdset_ptr(&rfds, &rfds_ptr, rfd_addr, n);
+    if (ret) {
+        return ret;
+    }
+    ret = copy_from_user_fdset_ptr(&wfds, &wfds_ptr, wfd_addr, n);
+    if (ret) {
+        return ret;
+    }
+
+    if (target_tv_addr) {
+        if (copy_from_user_timeval(&tv, target_tv_addr))
+            return -TARGET_EFAULT;
+        tv_ptr = &tv;
+    } else {
+        tv_ptr = NULL;
+    }
+
+    ret = get_errno(select(n, rfds_ptr, wfds_ptr, NULL, tv_ptr));
+
+    if (!is_error(ret)) {
+        if (rfd_addr && copy_to_user_fdset(rfd_addr, &rfds, n))
+            return -TARGET_EFAULT;
+        if (wfd_addr && copy_to_user_fdset(wfd_addr, &wfds, n))
+            return -TARGET_EFAULT;
+    }
+
+    return ret;
+}
+
 /* do_syscall() should always have a single exit point at the end so
    that actions, such as logging of syscall results, can be performed.
    All errnos that do_syscall() returns must be -TARGET_<errcode>. */
@@ -109,6 +241,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
 {
     /* CPUState *cpu = ENV_GET_CPU(cpu_env); */
     abi_long ret;
+    abi_ulong i;
     void *p;
 
 #ifdef DEBUG
@@ -128,7 +261,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
             goto efault;
         ret = get_errno(write(arg1, p, arg3));
         if (!is_error(ret)) {
-	        if (put_user_sal(ret, arg4))
+	        if (arg4 && put_user_sal(ret, arg4))
 		        goto efault;
 	        ret = 0;
         }
@@ -136,20 +269,57 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
         break;
 
     case 3: /* receive */
+	    if (!(p = lock_user(VERIFY_WRITE, arg2, arg3, 0)))
+		    goto efault;
 	    if (arg3 == 0)
 		    ret = 0;
-	    else {
-		    if (!(p = lock_user(VERIFY_WRITE, arg2, arg3, 0)))
-			    goto efault;
+		else
 		    ret = get_errno(read(arg1, p, arg3));
-		    if (!is_error(ret)) {
-			    if (put_user_sal(ret, arg4))
-				    goto efault;
-			    ret = 0;
-		    }
-		    unlock_user(p, arg2, ret);
+	    if (!is_error(ret)) {
+		    if (arg4 && put_user_sal(ret, arg4))
+			    goto efault;
+		    ret = 0;
 	    }
+	    unlock_user(p, arg2, ret);
 	    break;
+
+	case 4: /* fdwait */
+		ret = do_select(arg1, arg2, arg3, arg4);
+		if (ret >= 0) {
+			if (arg5 && put_user_sal(ret, arg5))
+				goto efault;
+			ret = 0;
+		}
+		break;
+
+	case 5: /* allocate */
+        if (!access_ok(VERIFY_WRITE, arg3, sizeof(abi_long)))
+            goto efault;
+        ret = get_errno(target_mmap(0, arg1, PROT_READ | PROT_WRITE | (arg2 ? PROT_EXEC: 0),
+                                    MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+        if (!is_error(ret)) {
+            if (put_user_sal(ret, arg3))
+                goto efault;
+            ret = 0;
+        }
+        break;
+
+    case 6: /* deallocate */
+        ret = get_errno(target_munmap(arg1, arg2));
+        break;
+
+	case 7: /* random */
+        if (!(p = lock_user(VERIFY_WRITE, arg1, arg2, 0)))
+            goto efault;
+        for (i = 0; i < (abi_ulong)arg2; i++) {
+            /* FIXME: Use same random algorithm as DARPA kernel */
+            ((char*)p)[i] = (char)(rand() & 0xff);
+        }
+        if (arg3 && put_user_sal(arg2, arg3))
+            goto efault;
+        ret = 0;
+        unlock_user(p, arg1, ret);
+        break;
 
     default:
         gemu_log("qemu: Unsupported syscall: %d\n", num);
